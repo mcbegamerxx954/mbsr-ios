e301d226f293b61934219bc0de37ab86e3c7f574
mcbegamerxx954 <154642722+mcbegamerxx954@users.noreply.github.com>
2024-02-23 00:00:00 +0000

    Upload

---
+++ src/lib.rs
@@ -0,0 +1,89 @@
+mod common;
+mod mc_utils;
+
+use icrate::Foundation::{NSHomeDirectory, NSString};
+use objc2::rc::{autoreleasepool, Id};
+use once_cell::sync::Lazy;
+use oslog::OsLogger;
+use std::collections::HashMap;
+use std::ffi::{OsStr, OsString};
+use std::path::{Path, PathBuf};
+use std::sync::Mutex;
+use std::thread;
+
+static SHADER_PATHS: Lazy<Mutex<HashMap<OsString, PathBuf>>> =
+    Lazy::new(|| Mutex::new(HashMap::new()));
+
+fn get_path() -> PathBuf {
+    let result = autoreleasepool(|pool| {
+        let sandbox_dir = unsafe {NSHomeDirectory().as_ref()};
+        let sandbox_dir = sandbox_dir.as_str(pool).to_owned();
+        let mut pathbuf = PathBuf::from(sandbox_dir);
+        pathbuf.extend(["Documents", "games", "com.mojang", "minecraftpe"]);
+        pathbuf
+    });
+    result
+}
+#[no_mangle]
+unsafe extern "C" fn mbsr_ruststartup() {
+    OsLogger::new("com.minecraft")
+        .level_filter(log::LevelFilter::Debug)
+        .init()
+        .unwrap();
+
+    log::info!(target: "ModInit", "Rust thread starting up!");
+    let _handler = thread::spawn(|| {
+        common::setup_json_watcher(get_path());
+    }).join();
+    
+    match _handler {
+        Ok(_) => (),
+        Err(e) => match e.downcast_ref::<&'static str>() {
+            Some(e_str) => log::error!(target:"ModInit", "JsonWatch thread Error: {e_str}"),
+            None => log::error!(target: "ModInit", "Unknown error in thread.."),
+        },
+    }
+}
+
+// This can be done safely thanks to objc2 which tries very hard to make 
+// types zero sized and also make them compile as lowly as possible
+#[no_mangle]
+unsafe extern "C" fn mbsr_get_rep_path(
+    file_name: &NSString,
+    ext_name: &NSString
+) -> Id<NSString> {
+    // TODO: figure out how to return null to avoid retaining
+    // empty strings, which is VERY bad to do when this is used to
+    // get path to assets which might be run many times
+    let result = autoreleasepool(|pool| {
+        let rs_filename = file_name.as_str(pool);
+        let extension = ext_name.as_str(pool);
+        log::info!(target: "MBHook", "Operating on path: {rs_filename:#?}");
+        if !rs_filename.ends_with(".material.bin") 
+        && extension != ".material.bin" {
+            return log_dismiss("Path is not for a material bin {rs_filename:#?}");
+        }
+        let path = Path::new(rs_filename);
+        let Some(filename) = path.file_name() else {
+            return log_dismiss("Cant parse filename from path {rs_filename:#?}");
+        };
+        let Ok(shader_paths) = SHADER_PATHS.lock() else {
+            return log_dismiss("Preventing crash: {e:#?}");
+        };
+        if let Some(rep_path) = shader_paths.get(filename)  {
+            let Some(utf8_path) = rep_path.to_str() else {
+                return log_dismiss("Custom path is not utf8: {rep_path:#?}");
+            };
+            return NSString::from_str(utf8_path);
+        };
+        return log_dismiss("Dismissing path: {rs_filename:#?}");
+    });
+    result
+}
+
+#[inline(always)]
+fn log_dismiss(reason: &str) -> Id<NSString>{
+    log::warn!(target: "MBHook", "{reason}");
+    //Really wanting to get rid of this, objc calls are expensive
+    NSString::from_str("")
+}
---
+++ src/mc_utils.rs
@@ -0,0 +1,157 @@
+use serde::de::DeserializeOwned;
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::ffi::OsString;
+use std::path::{Path, PathBuf};
+use std::{fs, io};
+
+// Keeps track and manages data about the minecraft Resource Pack Structure
+pub struct DataManager {
+    valid_packs: Vec<ValidPack>,
+    valid_packs_path: PathBuf,
+    global_packs_path: PathBuf,
+}
+
+// A pack that minecraft verified as valid
+#[derive(Deserialize, Default)]
+#[serde(default)]
+pub struct ValidPack {
+    file_version: Option<u32>,
+    uuid: String,
+    path: String,
+}
+
+// A active global pack
+#[derive(Deserialize, Debug)]
+struct GlobalPack {
+    pack_id: String,
+    subpack: Option<String>,
+}
+
+#[derive(Debug)]
+pub enum DataError {
+    //    #[error("Getting minecraft dir failed")]
+    //    AppDirsError(#[from] app_dirs2::AppDirsError);
+    JsonError(serde_json::Error),
+    IoError(io::Error),
+}
+impl std::error::Error for DataError {}
+impl std::fmt::Display for DataError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::JsonError(serde_error) => {
+                write!(f, "Expected valid json: {:?}", serde_error)
+            }
+            Self::IoError(io_error) => {
+                write!(f, "Cant read json file: {:?}", io_error)
+            }
+        }
+    }
+}
+
+impl From<serde_json::Error> for DataError {
+    fn from(err: serde_json::Error) -> Self {
+        Self::JsonError(err)
+    }
+}
+
+impl From<io::Error> for DataError {
+    fn from(err: io::Error) -> Self {
+        Self::IoError(err)
+    }
+}
+impl DataManager {
+    // Get minecraft paths and create itself
+    pub fn init_data(mcjsons_dir: &Path) -> Self {
+        let valid_packs_path = mcjsons_dir.join("valid_known_packs.json");
+        let global_packs_path = mcjsons_dir.join("global_resource_packs.json");
+        Self {
+            valid_packs: Vec::new(),
+            valid_packs_path,
+            global_packs_path,
+        }
+    }
+
+    // Get valid packs from minecraft
+    pub fn update_validpacks(&mut self) -> Result<(), DataError> {
+        let mut valid_packs: Vec<ValidPack> = vec_from_json(&self.valid_packs_path)?;
+        if let Some(file_version) = valid_packs[0].file_version {
+            assert!(file_version == 2);
+            valid_packs.remove(0);
+        };
+        self.valid_packs = valid_packs;
+        Ok(())
+    }
+
+    // Get a list of shader paths
+    pub fn shader_paths(&self) -> Result<HashMap<OsString, PathBuf>, DataError> {
+        let global_packs: Vec<GlobalPack> = vec_from_json(&self.global_packs_path)?;
+        log::info!(target: "McUtils", "global_packs parsed: {:#?}", global_packs);
+        let mut final_paths = HashMap::new();
+        for pack in global_packs {
+            if let Some(vp) = self.valid_packs.iter().find(|vp| pack.pack_id == vp.uuid) {
+                let mut paths = match scan_pack(&vp.path, pack.subpack) {
+                    Ok(paths) => paths,
+                    Err(e) => {
+                        log::error!("scan paths error: {e}");
+                        continue;
+                    }
+                };
+                log::info!(target: "McUtils", "scan pack paths: {:#?}", &paths);
+                paths.retain(|k, _| !final_paths.contains_key(k));
+                log::info!(target: "McUtils", "unique paths are: {:#?}", &paths);
+                final_paths.extend(paths);
+            }
+        }
+
+        Ok(final_paths)
+    }
+    // Get shaders in pack directory
+}
+fn scan_pack(path: &str, subpack: Option<String>) -> Result<HashMap<OsString, PathBuf>, io::Error> {
+    log::trace!(target: "McUtils", "Scanning path: {}", path);
+    let path = Path::new(path);
+    let mut pack_files = scan_path(path)?;
+    if let Some(subpack) = subpack {
+        log::info!(target: "McUtils", "Scanning subpath: {}", &subpack);
+        let mut subpath = path.join("subpacks");
+        subpath.push(subpack);
+
+        let sub_files = scan_path(&subpath)?;
+        log::trace!(target: "McUtils", "expanding pack files with :{:#?}", &sub_files);
+        pack_files.extend(sub_files);
+    }
+    Ok(pack_files)
+}
+fn scan_path(path: &Path) -> Result<HashMap<OsString, PathBuf>, io::Error> {
+    let mut path = path.join("renderer");
+    path.push("materials");
+    let dir_entries = fs::read_dir(path)?;
+    let mut paths: HashMap<OsString, PathBuf> = HashMap::new();
+    for entry in dir_entries.flatten() {
+        let path = entry.path();
+        let file_name = entry.file_name();
+        // Some very important checks are done here
+        let metadata = entry.metadata()?;
+        // Check if len is larger than usize
+        #[cfg(target_os = "android")]
+        if metadata.len() >= usize::MAX as u64 {
+            continue;
+        }
+        if !metadata.is_file() {
+            continue;
+        }
+        let bytes = file_name.as_encoded_bytes();
+        if !paths.contains_key(&file_name) && bytes.ends_with(b".material.bin") {
+            log::info!(target: "McUtils", "scan_path found a valid shader path!: {:#?}", &path);
+            paths.insert(file_name, path);
+        }
+    }
+    Ok(paths)
+}
+
+pub(crate) fn vec_from_json<T: DeserializeOwned>(path: &Path) -> Result<Vec<T>, DataError> {
+    let json_file = fs::read_to_string(path)?;
+    let json_vec: Vec<T> = serde_json::from_str(&json_file)?;
+    Ok(json_vec)
+}
